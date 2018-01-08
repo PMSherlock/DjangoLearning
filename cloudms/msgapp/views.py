1d59f235ae9ec0b9bc16fc1e788c90a896babf8c
PMSherlock <wsliang90@163.com>
2018-01-08 22:37:50 +0800

    编写了部分视图函数，现在还会报错

--- cloudms/msgapp/views.py
+++ cloudms/msgapp/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def msgproc(request):
-    # if request.method == 'POST':
+
+    datalist = []
+
+    if request.method == 'POST':
+        userA = request.POST.get('userA', None)
+        userB = request.POST.get('userB', None)
+        msg = request.POST.get('msg', None)
+        time = datetime.now()
+
+        with open('data.txt', 'a+') as f:
+            f.write('{}--{}--{}--{}\n'.format(userA, userB, msg, time.strftime('%Y-%m-%d %H:%M:%S')))
+
+
+    if request.method == 'GET':
+        userC = request.GET.get('userC', None)
+
+        with open('data.txt', 'r') as f:
+            for line in f:
+                linedata = line.split('--')
+                d = {'userA':linedata[1],  'msg':linedata[2], 'time':linedata[3]}
+                datalist.append(d)
+
     return render(request,'index.html')
