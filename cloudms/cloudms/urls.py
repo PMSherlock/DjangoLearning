f05091b641ba29ab4964ce2174feb88b7968b5b3
PMSherlock <wsliang90@163.com>
2018-01-15 22:40:09 +0800

    修改了路径

--- cloudms/cloudms/urls.py
+++ cloudms/cloudms/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from msgapp import views
 
 urlpatterns = [
+    path('',views.index ),
     path('msggate', include('msgapp.urls')),
     path('admin/', admin.site.urls),
 ]
